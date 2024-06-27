59ad37cc1e893437e526e892d28783d3d37c3773
JuanSudo <u202211414@upc.edu.pe>
2024-06-27 12:46:19 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -45,4 +45,62 @@ document.addEventListener('DOMContentLoaded', function() {
         reportModal.style.display = 'none';
         reportForm.reset();
     });
+
+
+    // Aquí comienza la inicialización del mapa
+    let map;
+
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: -12.0464, lng: -77.0428 }, // Coordenadas iniciales del mapa
+            zoom: 15 // Nivel de zoom inicial
+        });
+
+        const input = document.getElementById('searchInput');
+        const searchBox = new google.maps.places.SearchBox(input);
+
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+        map.addListener('bounds_changed', function() {
+            searchBox.setBounds(map.getBounds());
+        });
+
+        let markers = [];
+        searchBox.addListener('places_changed', function() {
+            const places = searchBox.getPlaces();
+
+            if (places.length === 0) {
+                return;
+            }
+
+            markers.forEach(function(marker) {
+                marker.setMap(null);
+            });
+            markers = [];
+
+            const bounds = new google.maps.LatLngBounds();
+            places.forEach(function(place) {
+                if (!place.geometry) {
+                    console.log("Returned place contains no geometry");
+                    return;
+                }
+
+                markers.push(new google.maps.Marker({
+                    map: map,
+                    title: place.name,
+                    position: place.geometry.location
+                }));
+
+                if (place.geometry.viewport) {
+                    bounds.union(place.geometry.viewport);
+                } else {
+                    bounds.extend(place.geometry.location);
+                }
+            });
+            map.fitBounds(bounds);
+        });
+    }
+
+    // Llama a la función de inicialización del mapa al cargar la página
+    initMap();
 });
