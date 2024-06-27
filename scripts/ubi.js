abea383ac7ad608877416679712f782a62dc1878
JuanSudo <u202211414@upc.edu.pe>
2024-06-27 12:19:07 -0500

    Update ubi.js

--- scripts/ubi.js
+++ scripts/ubi.js
@@ -1,1 +1,42 @@
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 10,
+        center: { lat: -34.397, lng: 150.644 }
+    });
 
+    const input = document.getElementById("searchInput");
+    const searchBox = new google.maps.places.SearchBox(input);
+
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    map.addListener("bounds_changed", () => {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    searchBox.addListener("places_changed", () => {
+        const places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        const bounds = new google.maps.LatLngBounds();
+
+        places.forEach(place => {
+            if (!place.geometry || !place.geometry.location) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+
+            if (place.geometry.viewport) {
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+
+        map.fitBounds(bounds);
+    });
+}
+
+window.onload = initMap;
